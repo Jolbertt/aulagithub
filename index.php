da7264e8ac257362db565ea63095ac7c87edc0eb
Jolbertt Siqueira <jolbertts@gmail.com>
2023-07-17 21:14:03 -0300

    Feito modificações para o terceiro commit do desafio

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+function terceiroCommit(){
+    $ret = array(
+        0 => "Terceiro",
+        1 => "Commit"
+    );
+    return $ret;
+}
+
 echo("Pagina de teste para o curso de github<\br>");
-echo("Primeiro commit!!");
-print_r("Esta é a modificação para o segundo commit do curso de github");
+echo("Primeiro commit!!<\br>");
+print_r("Esta é a modificação para o segundo commit do curso de github<\br>");
+print_r(terceiroCommit());
